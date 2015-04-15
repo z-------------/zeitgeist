b87fe5373bfe4a9a8d8272a28bfd2816cbbeed5a
Zack Guard <zacharyguard@gmail.com>
2015-04-15 09:00:06 +0800

    Edit table values using editor

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var zeitgeist = {};
-zeitgeist.currentPeriod 
+/* basic functions */
 
 var zpad = function(str, len){
     if (typeof str !== "string") str = str.toString();
@@ -10,50 +9,76 @@ Array.prototype.append = function(arr){
     return [].push.apply(this, arr);
 };
 
-var WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-var TIMES = ["08:40", "09:45", "10:50", "11:15", "12:15", "1:15", "2:15", "3:15"];
-
-var namedPeriods = {
-    2: "First break",
-    4: "Long break",
-    7: "End of day"
+HTMLElement.prototype.hide = function(){
+    return this.setAttribute("hidden", "true");
 };
 
-var editorElem = document.querySelector("#editor-container");
+HTMLElement.prototype.show = function(){
+    return this.removeAttribute("hidden");
+};
 
-if (!localStorage.getItem("zeitgeist_table")) {
+NodeList.prototype.each = function(callback){
+    return [].slice.call(this).forEach(callback);
+};
+
+/* define elements */
+
+var editor = {};
+editor.container = document.querySelector(".editor");
+editor.table = editor.container.querySelector(".editor_table");
+editor.saveBtn = editor.container.querySelector(".editor_save");
+
+/* define globals */
+
+var zeit = {};
+zeit.LOCALSTORAGE_VARNAME = "zeitgeist_table";
+
+/* zeitgeist is go */
+
+editor.saveBtn.addEventListener("click", function(){
+    // save table contents into localStorage
     var table = [];
-
-    for (var d = 0; d < WEEKDAYS.length; d++) {
-        var day = {};
-        day.day = WEEKDAYS[d];
-        day.periods = [];
-        for (var p = 0; p < TIMES.length; p++) {
-            var period = {};
-            period.name = "Untitled period";
-            if (p in namedPeriods) period.name = namedPeriods[p];
-            period.time = TIMES[p];
-            day.periods.push(period);
-        }
-        table.push(day);
-    }
     
-    localStorage.setItem("zeitgeist_table", JSON.stringify(table));
+    var rows = editor.table.querySelectorAll("tr");
+    rows.each(function(row, i){
+        var day = {};
+        day.index = i;
+        day.periods = [];
+        
+        var tds = row.querySelectorAll("td");
+        tds.each(function(elem){
+            var nameInput = elem.querySelector("input");
+            
+            var period = {
+                time: "00:00",
+                name: nameInput.value
+            };
+            
+            day.periods.push(period);
+        });
+        
+        table.push(day);
+    });
+    
+    localStorage.setItem(zeit.LOCALSTORAGE_VARNAME, JSON.stringify(table));
+});
+
+if (!localStorage.getItem(zeit.LOCALSTORAGE_VARNAME)) {
+    editor.container.show();
+} else {
+    zeit.table = JSON.parse(localStorage.getItem(zeit.LOCALSTORAGE_VARNAME));
+    console.log(zeit);
 }
 
-var table = JSON.parse(localStorage.getItem("zeitgeist_table"));
-console.log(table);
+/* initialize editor */
 
-table.forEach(function(day){
+for (var d = 0; d < 7; d++) {
     var dayElem = document.createElement("tr");
-    day.periods.forEach(function(period, i){
+    for (var p = 0; p < 5; p++) {
         var periodElem = document.createElement("td");
-        periodElem.innerHTML = "<input type='text'>";
-        periodElem.querySelector("input").value = period.name;
-        if (i in namedPeriods) {
-            periodElem.querySelector("input").setAttribute("readonly", "true");
-        }
+        periodElem.innerHTML = "<input type='text' class='editor_periodname' placeholder='Period name'>";
+        periodElem.querySelector(".editor_periodname").value = (zeit.table[d].periods[p] ? zeit.table[d].periods[p].name : null);
         dayElem.appendChild(periodElem);
-    });
-    editorElem.querySelector("table").appendChild(dayElem);
-});
+    }
+    editor.table.appendChild(dayElem);
+}
